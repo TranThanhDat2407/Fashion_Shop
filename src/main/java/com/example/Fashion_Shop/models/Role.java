055cbaaeff38d18468bcaaa274948b083ccea215
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-11-11 15:28:39 +0700

    replace dependency, add role model

--- src/main/java/com/example/Fashion_Shop/models/Role.java
+++ src/main/java/com/example/Fashion_Shop/models/Role.java
@@ -1,12 +1,19 @@
 package com.example.Fashion_Shop.models;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table( name = "Roles")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
 }
--- src/main/java/com/example/Fashion_Shop/models/User.java
+++ src/main/java/com/example/Fashion_Shop/models/User.java
@@ -1,13 +1,23 @@
 package com.example.Fashion_Shop.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User extends BaseEntity{
-    private int id;
+    private Integer id;
     private String name;
     private String email;
     private String phone;
-    private int role_id;
+    private Integer role_id;
     private String password;
     private String one_time_password;
     private Date otp_requested_time;
